a72fe27b8b3f31f715a3d85ded77afae386d05da
yeadr <yussef.elalami@institutvidreres.cat>
2025-11-21 09:14:31 +0100

    Ex 2

--- Yussef_Ex4.js
+++ Yussef_Ex4.js
@@ -1,5 +1,10 @@
 function ex1() {
-const arr = [Math.random(), Math.random(), Math.random(), Math.random(), Math.random()]
+console.log("Exercici 1:")
+const arr = [Math.trunc(Math.random() * 1000) / 1000, 
+            Math.trunc(Math.random() * 1000) / 1000, 
+            Math.trunc(Math.random() * 1000) / 1000, 
+            Math.trunc(Math.random() * 1000) / 1000, 
+            Math.trunc(Math.random() * 1000) / 1000]
 
 const ordenat = [...arr].sort((a, b) => {
     return a-b
@@ -11,3 +16,13 @@ const ultim = ordenat[ordenat.length-1]
 return {arr, ordenat, primer, ultim}
 }
 console.log(ex1())
+
+function ex2() {
+console.log("")
+console.log("Exercici 2:")
+const ordenat = ex1().ordenat
+const nou = [...ordenat]
+return nou
+}
+
+console.log(ex2())
